50e875054d8e52eeb71d541f4d1143e289c98ea3
VidyaChandrub <vidyac266@gmail.com>
2022-12-08 19:31:29 +0530

    developer

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      
+    <Navbar/>
+    <HeroImg2 heading="PROJECTS." text="Some of my most recent projects."/>
+    <Footer />
+    </div>
   )
 }
 
