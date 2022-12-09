1acd21377e78a6af7cebb61af1055c4295ee36af
VidyaChandrub <vidyac266@gmail.com>
2022-12-09 13:23:25 +0530

    cards

---
+++ src/components/WorkCardData.jsx
@@ -0,0 +1,43 @@
+import pro1 from '../assets/project1.jpg'
+import pro2 from '../assets/project2.jpg'
+import pro3 from '../assets/project3.jpg'
+
+const ProjectCardData = [
+    {
+        imgsrc : pro1,
+        title: "Ecommerce Website Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } ,
+    {
+        imgsrc : pro2,
+        title: "LMS/Course Website Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } ,
+    {
+        imgsrc : pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } ,
+    {
+        imgsrc : pro1,
+        title: "Ecommerce Website Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } ,
+    {
+        imgsrc : pro2,
+        title: "LMS/Course Website Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } ,
+    {
+        imgsrc : pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit sunt nam quibusdam quos nobis, sequi neque sapiente deserunt debitis repellat nulla harum quidem suscipit maiores. Laborum earum perferendis inventore reprehenderit!",
+        view :"https://youtu.be/0h2b4ftbZcU"
+    } 
+]
+export default ProjectCardData
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import PricingCard from '../components/PricingCard'
+import Work from '../components/Work'
 
 const Projects = () => {
   return (
@@ -10,6 +11,7 @@ const Projects = () => {
       
     <Navbar/>
     <HeroImg2 heading="PROJECTS." text="Some of my most recent projects."/>
+    <Work />
     <PricingCard />
     <Footer />
     </div>
