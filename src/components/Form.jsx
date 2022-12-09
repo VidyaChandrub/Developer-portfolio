ac10c0e6892e038327bc30b2a93cc3ad894e9e58
VidyaChandrub <vidyac266@gmail.com>
2022-12-09 22:33:06 +0530

    complete-portfolio

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,23 @@
-import './FormStyles.css'
+import './FormStyles.css';
+
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='form'>
+        <form>
+            <label>Your Name </label>
+            <input type="text" />
+            <label>Email</label>
+            <input type="email" />
+            <label>Subject </label>
+            <input type="text" />
+            <label>Message </label>
+            <textarea row="6" placeholder='Type your message here'></textarea>
+            <button className='btn'>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
