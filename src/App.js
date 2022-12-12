7e551875db08e879953e1845e5763af965cbdf4a
VidyaChandrub <vidyac266@gmail.com>
2022-12-12 14:33:00 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from 'react'
-
-import Employee from './team-allocation/Employee'
-import Footer from './team-allocation/Footer'
-import Header from './team-allocation/Header' 
-//  import App1 from './portfolio/App1'
+import React from 'react' 
+import App1 from './portfolio/App1'
 function App() {
   return (
     <div>
-   <Header/>
-   <Employee />
-   <Footer />
-    {/* <App1 />  */}
+   
+   <App1 /> 
 
     </div>
   )
